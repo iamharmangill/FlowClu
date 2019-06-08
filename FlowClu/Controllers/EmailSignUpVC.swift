2513a627bdd29f50563ed38794d5810cafec6dc6
Harman Gill <harmangill@ip-192-168-50-33.us-west-2.compute.internal>
2019-06-07 21:55:15 -0700

    Basic Login UI

---
+++ FlowClu/Controllers/EmailSignUpVC.swift
@@ -0,0 +1,30 @@
+//
+//  EmailSignUpVC.swift
+//  FlowClu
+//
+//  Created by Harman Gill on 2019-06-07.
+//  Copyright © 2019 Harman Gill. All rights reserved.
+//
+
+import UIKit
+
+class EmailSignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
